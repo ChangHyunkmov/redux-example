6117319df0fd21518dbd5ce418a4865e19bf4c69
KCH <hot9998@naver.com>
2020-06-23 16:26:03 +0900

    redux-example

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,4 @@
+// 액션의 이름을 상수로 만든다
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const SET_COLOR = "SET_COLOR";
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Counter from "./Counter";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <Counter></Counter>;
+  }
+}
